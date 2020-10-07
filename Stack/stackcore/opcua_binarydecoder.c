LeSpocky/UA-AnsiC

commit c0328167977ba853ae8da512249eef75cdfdd8ff

[WORKAROUND] change OpcUa_BinaryDecoder_ReadExtensionObject(...)
workaround when name of namespace didn't found:
* set index of namespace to zero
* return OpcUa_Good

Suggested-by: Horst Schumann

* add logging macro to create warning on matching loglevel

Stack/stackcore/opcua_binarydecoder.c
@@ -28,6 +28,8 @@
 #include <opcua_binaryencoder.h>
 #include <opcua_binaryencoderinternal.h>
 
+#include <ttlog_OPCUA_logmacro.h>
+
 /*============================================================================
  * OpcUa_BinaryDecoder
  *
@@ -1606,7 +1608,16 @@ OpcUa_StatusCode OpcUa_BinaryDecoder_ReadExtensionObject(
                 &a_pValue->TypeId.NamespaceUri,
                 &nIndex);
 
-            OpcUa_GotoErrorIfBad(uStatus);
+            /*            OpcUa_GotoErrorIfBad(uStatus); */
+            if (0 != uStatus)
+			{
+				OPCUA_WARN_WRITE("OpcUa_StringTable_FindIndex failed for \"%s\""
+						" in OpcUa_BinaryDecoder_ReadExtensionObject(). "
+						"Setting index to 0.\n",
+						OpcUa_String_GetRawString(&a_pValue->TypeId.NamespaceUri));
+				nIndex = 0;
+				uStatus = OpcUa_Good;
+			}
 
             a_pValue->TypeId.NodeId.NamespaceIndex = (OpcUa_UInt16)nIndex;
             OpcUa_String_Clear(&a_pValue->TypeId.NamespaceUri);

commit 62d44a16600557cee63c17a60171aeea43fc4775

stackcore: binarydecoder: Use internal logging mechanism
Our commit:c0328167977b broke standalone build of the stack back in
February of 2019.  Later that year we learned how to hook into the
"Trace" mechanism of the stack to let the application log those traces
of the stack.  In commserver that was with v0.0.3-57-g66cc054
(2019-07-08).  So we can use 'OpcUa_Trace()' here to get our information
out, the CommServer is able to display that somehow now, no need to
include header files not part of this stack anymore.

Note: the functional workaround is still in place, this just fixes the
logging part!

Fixes: commit:c0328167977b ("[WORKAROUND] change OpcUa_BinaryDecoder_ReadExtensionObject(...)")

Stack/stackcore/opcua_binarydecoder.c
@@ -28,8 +28,6 @@
 #include <opcua_binaryencoder.h>
 #include <opcua_binaryencoderinternal.h>
 
-#include <ttlog_OPCUA_logmacro.h>
-
 /*============================================================================
  * OpcUa_BinaryDecoder
  *
@@ -1611,10 +1609,11 @@ OpcUa_StatusCode OpcUa_BinaryDecoder_ReadExtensionObject(
             /*            OpcUa_GotoErrorIfBad(uStatus); */
             if (0 != uStatus)
 			{
-				OPCUA_WARN_WRITE("OpcUa_StringTable_FindIndex failed for \"%s\""
-						" in OpcUa_BinaryDecoder_ReadExtensionObject(). "
-						"Setting index to 0.\n",
-						OpcUa_String_GetRawString(&a_pValue->TypeId.NamespaceUri));
+				OpcUa_Trace(OPCUA_TRACE_LEVEL_WARNING,
+                            "OpcUa_StringTable_FindIndex failed for \"%s\""
+                            " in OpcUa_BinaryDecoder_ReadExtensionObject(). "
+                            "Setting index to 0.\n",
+                            OpcUa_String_GetRawString(&a_pValue->TypeId.NamespaceUri));
 				nIndex = 0;
 				uStatus = OpcUa_Good;
 			}